1d5804adae3919edbf8c3608a3c6020ae0c70100
SSDN <administrator@ssdn-macbook.local>
2026-02-22 15:27:41 +0530

    Fixes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,6 @@
-import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage(true);
-  };
-
   const communicationChannel = [
     {
       id: 1,
